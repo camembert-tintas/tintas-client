575f335783ec3efb4cce4dde4aa924518b3f46f7
MaximeSauvage1 <maxime.sauvage1@gmail.com>
2017-12-14 09:40:46 +0100

    [client][Engine] add test for Engine - plateau generator

---
+++ src/test/java/EngineTest.java
@@ -0,0 +1,44 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import core.*;
+
+public class EngineTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testConstructor() {
+		int count = 0;
+	    String letter = "ABCDEFGHI";
+	    for (int i = 0; i < letter.length(); i++) {
+	        for (int j = 1; j <= 9; j++) {
+	            Coordinate coordinates = new Coordinate(letter.charAt(i), j);
+	            if (coordinates.isValid()) {
+	                count++;
+	            }
+	        }
+	    }
+	    assertTrue(count == 49);
+	}
+
+}
