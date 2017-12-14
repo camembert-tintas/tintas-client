6f921ff5cc45ab0f3b4ef3318985e493f7981c8b
Bryan Sandras <Bryan Sandras>
2017-12-14 15:21:46 +0100

    impl du main

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -1,12 +1,21 @@
 package core;
 
+import java.util.Scanner;
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		Scanner sc = new Scanner(System.in);
+		Engine jeu = new Engine();
+		jeu.addPlayer(new Player("Player 1"));
+		while(!jeu.getPlayers().get(0).check7PiecesSameColor() && 
+				!jeu.getPlayers().get(0).check7PiecesSameColor()){
+			System.out.print("Entrer une coordonnée: ");
+			String coord = sc.nextLine();
+			System.out.println(coord);
+		}
 	}
-
 }
