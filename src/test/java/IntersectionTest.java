e49986c1eb02ee961483f7c7c9ec30074475e49c
Bryan Sandras <Bryan Sandras>
2017-12-14 09:43:26 +0100

    test de l'aajout d'une piece

--- src/test/java/IntersectionTest.java
+++ src/test/java/IntersectionTest.java
@@ -6,14 +6,9 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-<<<<<<< HEAD
 
-//import core.Intersection;
-=======
 import utility.State;
-import core.Coordinate;
-import core.Intersection;
->>>>>>> devBryan
+import core.*;
 
 public class IntersectionTest {
 
@@ -35,18 +30,15 @@ public class IntersectionTest {
 
 	@Test
 	public void test() {
-<<<<<<< HEAD
-//		Intersection inter = new Intersection();
-//		assertEquals(inter.getState());
-//		assertEquals(inter.getColor(), null);
-//		assertEquals(inter.getCoord(), null);
-=======
 		Coordinate c = new Coordinate('A', 1);
 		Intersection inter = new Intersection(c);
 		assertEquals(inter.getState(), State.LIBRE.getState());
 		assertEquals(inter.getCoord().toString(), c.toString());
 		assertEquals(inter.getPiece(), null);
->>>>>>> devBryan
+		Piece p = new Piece();
+		inter.addPiece(p);
+		assertEquals(inter.getState(), State.PIECE.getState());
+		assertEquals(inter.getPiece().getColor(), p.getColor());
 	}
 
 }
