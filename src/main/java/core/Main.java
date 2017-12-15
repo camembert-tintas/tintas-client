31acc0ea0cae1eec51e4a61120767457c13af1ac
MaximeSauvage1 <maxime.sauvage1@gmail.com>
2017-12-15 13:58:32 +0100

    [client] update BDD at the end of the game

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -1,6 +1,12 @@
 package core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
+
+import utility.DataBaseConnection;
 
 public class Main {
 
@@ -10,18 +16,46 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Engine e = new Engine();
-		e.addPlayer(new Player("Marcus"));
-		e.addPlayer(new Player("Romulus"));
+		System.out.print("Entrer le même login que celui de l'inscription: ");
+		String name = sc.nextLine();
+		e.addPlayer(new Player(name));
+		e.addPlayer(new Player("IA manuel"));
 		e.setPawn(new Coordinate('A',6));
 		e.printTableau();
 		do{						
 			System.out.println(e.getPlayers().get(e.actualPlayer()).getName());
-			System.out.print("Entrer une coordonn�e(exemple: B6): ");
+			System.out.print("Entrer une coordonnee(exemple: B6): ");
 			//String coord = sc.nextLine();
 			//e.move(new Coordinate(Character.toUpperCase(coord.charAt(0)), Character.getNumericValue(coord.charAt(1))));
 			e.randomMove();
 			e.printTableau();
 		}while(!e.hasWinner());
 		System.out.println("FIN!");
+		
+		try{
+			Connection connexion;
+			PreparedStatement preparedStatement;
+			
+			connexion = DataBaseConnection.connect();
+			
+			String subRequestRes = "SELECT id FROM player WHERE login =\""+name+"\"";
+			preparedStatement = connexion.prepareStatement(subRequestRes);
+			ResultSet resultId = preparedStatement.executeQuery();
+			resultId.next();
+			int playerId = resultId.getInt("id");
+			
+			int gameId = e.getId(), score = e.getScore(name), isWinner = e.getWinner().equals(name)?1:0;
+			String request = "INSERT INTO games(games_id,player_id,player_score,player_win) VALUES("+
+					gameId + "," + playerId + "," + score + "," + isWinner +")";
+			
+			preparedStatement = connexion.prepareStatement(request);
+			
+			preparedStatement.close();
+			connexion.close();
+			
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+			System.out.println("Veulliez vous inscrire sur le site.");
+		}
 	}
 }
